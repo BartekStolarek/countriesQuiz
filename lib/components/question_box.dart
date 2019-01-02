c70d6aa3a892d0d6bfbc76a48afe54019db36355
BartekStolarek <bartek.stolarek@gmail.com>
2019-01-02 15:51:33 +0100

    Updated README.md and moved files

--- lib/components/question_box.dart
+++ lib/components/question_box.dart
@@ -18,7 +18,10 @@ class QuestionBox extends StatelessWidget {
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                new Text('Which country has this currency?', style: new TextStyle(color: Colors.white, fontSize: 25.0)),
+                new Padding(
+                  padding: new EdgeInsets.only(top: 5.0),
+                  child: new Text('Whose currency is it?', style: new TextStyle(color: Colors.white, fontSize: 25.0))
+                ),
                 new Padding(
                   padding: new EdgeInsets.only(top: 25.0),
                   child: new Text(_question, style: new TextStyle(color: Colors.white, fontSize: 75.0, fontWeight: FontWeight.bold))
